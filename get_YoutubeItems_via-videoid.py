3d485b116c58f9eb399a8ac4eb219e7ce05c1877
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-02-21 23:42:06 +0900

    Create get_YoutubeItems_via-videoid.py
    動画IDから取得
    
    ```json
    {
    "publishedAt": "2009-11-21T12:30:25Z",
    "channelId": "UC812_xRhpLCUc-3g0LNbrMQ",
    "title": "～蛍の光～閉店の時間",
    "description": "この曲はどこのお店にもありますよねｗ\r\n\r\n\r\n画像は自分で作りましたｗ",
    "thumbnails": {
    "default": {
    "url": "https://i.ytimg.com/vi/OgYWssWn7uQ/default.jpg",
    "width": 120,
    "height": 90
    },
    "medium": {
    "url": "https://i.ytimg.com/vi/OgYWssWn7uQ/mqdefault.jpg",
    "width": 320,
    "height": 180
    },
    "high": {
    "url": "https://i.ytimg.com/vi/OgYWssWn7uQ/hqdefault.jpg",
    "width": 480,
    "height": 360
    },
    "standard": {
    "url": "https://i.ytimg.com/vi/OgYWssWn7uQ/sddefault.jpg",
    "width": 640,
    "height": 480
    }
    },
    "channelTitle": "kazuyuki93",
    "tags": [
    "閉店",
    "時間",
    "蛍の光"
    ],
    "categoryId": "10",
    "liveBroadcastContent": "none",
    "localized": {
    "title": "～蛍の光～閉店の時間",
    "description": "この曲はどこのお店にもありますよねｗ\r\n\r\n\r\n画像は自分で作りましたｗ"
    }
    }
    
    ```

---
+++ get_YoutubeItems_via-videoid.py
@@ -0,0 +1,34 @@
+import json
+from apiclient.discovery import build
+
+def load_config():
+    config = None
+    with open('.secret/config.json') as f:
+        config = json.load(f)
+    return config
+
+config = load_config()
+
+# Youtube APIトークン
+YOUTUBE_API_KEY = config['external']['youtube']['api_key']
+
+# Youtube動画ID
+videoId = 'OgYWssWn7uQ'
+
+youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
+videos_response = youtube.videos().list(
+    part='snippet,statistics',
+    id='{},'.format(videoId)
+).execute()
+# snippet
+snippetInfo = videos_response["items"][0]["snippet"]
+# 動画タイトル
+title = snippetInfo['title']
+# チャンネル名
+channeltitle = snippetInfo['channelTitle']
+print(channeltitle)
+print(title)
+print(f'https://www.youtube.com/watch?v={videoId}')
+
+json_file=open('test.json', 'w')
+print(json.dump(snippetInfo,json_file))
