b58d729966ea9e10ccf1a693a725d12548c262ee
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-02-21 11:47:21 +0900

    Update youtube_bot.py

--- youtube_bot.py
+++ youtube_bot.py
@@ -36,20 +36,19 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    # Ping値を測定 [Ping値を測定](https://discordbot.jp/blog/16/)
-    if message.content == "/ping":
-        # Ping値を秒単位で取得
-        raw_ping = client.latency
-
-        # ミリ秒に変換して丸める
-        ping = round(raw_ping * 1000)
-
-        # 送信する
-        await message.reply(f"Pong!\nBotのPing値は{ping}msです。")
-    if message.content == "/version":
-        await message.reply(print_version())
-        
     try:
+        # Ping値を測定 [Ping値を測定](https://discordbot.jp/blog/16/)
+        if message.content == "!ping":
+            # Ping値を秒単位で取得
+            raw_ping = client.latency
+
+            # ミリ秒に変換して丸める
+            ping = round(raw_ping * 1000)
+
+            # 送信する
+            await message.reply(f"Pong!\nBotのPing値は{ping}msです。")
+        if message.content == "!version":
+            await message.reply(f"Current version is below.\ndiscordpy:\t"+discord.__version__)
     except:
         sys.exit()
 
