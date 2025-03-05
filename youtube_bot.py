b6ad212c5899758ce32cdb88015602f2b7a8dc8b
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-03-05 12:46:31 +0900

    Update youtube_bot.py

--- youtube_bot.py
+++ youtube_bot.py
@@ -413,11 +413,8 @@ async def on_ready():
             print(file)
             if not(os.path.isdir(os.path.dirname(file))):
                 os.mkdir(os.path.dirname(file))
-            if os.path.isfile(file):
-                with open(file,encoding='UTF-8',mode='w') as f:
-                    f.write(response)
-            else:
-                print('os.path.isfile(file):{}'.format(os.path.isfile(file)))
+            with open(file,encoding='UTF-8',mode='w') as f:
+                f.write(response)
             print('Discord channel({0})に起動メッセージ送信完了'.format( channel_id ))
         except Exception as e:
             print(e)
