e8a8cbee4f1dc8b9196cb3068001173912391be3
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-03-05 11:44:53 +0900

    Update youtube_bot.py

--- youtube_bot.py
+++ youtube_bot.py
@@ -399,6 +399,7 @@ async def on_ready():
             color=0x00ff00,
             url=GLOBAL_TEXT['url']['github']['repository'],
         )
+        embed.set_thumbnail(client.avatar.url)
         await channel.send(embed=embed)
         print('Discord channel({0})に起動メッセージ送信完了'.format( channel_id ))
 
