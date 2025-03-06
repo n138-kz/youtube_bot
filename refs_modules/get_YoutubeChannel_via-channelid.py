625698405cabea797787cff7fb578556116b1841
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-03-06 20:52:14 +0900

    Update get_YoutubeChannel_via-channelid.py

--- refs_modules/get_YoutubeChannel_via-channelid.py
+++ refs_modules/get_YoutubeChannel_via-channelid.py
@@ -1,9 +1,19 @@
+import os
 import json
 from apiclient.discovery import build
 
 def load_config():
     config = None
-    with open('.secret/config.json') as f:
+    # 現在のスクリプトファイルのディレクトリを取得
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # 1つ上の階層のディレクトリパスを取得
+    parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+
+    # 1つ上の階層にあるファイルのパスを作成
+    file_path = os.path.join(parent_dir, '.secret/config.json')
+
+    with open(file_path) as f:
         config = json.load(f)
     return config
 
@@ -13,7 +23,7 @@ config = load_config()
 YOUTUBE_API_KEY = config['external']['youtube']['api_key']
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
-CHANNEL_ID = config['external']['youtube']['channel_id']
+CHANNEL_ID = config['internal']['youtube']['channel_id']
 
 youtube = build(
     YOUTUBE_API_SERVICE_NAME,
@@ -25,6 +35,9 @@ response = youtube.channels().list(
     part='snippet,statistics',
     id=CHANNEL_ID
 ).execute()
+
+with open(os.path.splitext(os.path.basename(__file__))[0]+'.json',mode='w',encoding='UTF-8') as f:
+    json.dump(obj=response,fp=f, indent=2, ensure_ascii=False)
 
 for item in response.get("items", []):
     if item["kind"] != "youtube#channel":
