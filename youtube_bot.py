05179f1d22ded886a27ab1185c0764cebc18e5fb
Komiya Yu <komiya.yu@n138.jp>
2025-04-04 04:40:52 +0900

    filename

--- youtube_bot.py
+++ youtube_bot.py
@@ -884,7 +884,7 @@ async def on_message(message):
                         embed.add_field(inline=False,name=item,value='```\n{}```'.format(None))
                     except KeyError:
                         embed.add_field(inline=False,name=item,value='```\n{}```'.format(None))
-                response=await message.reply(embed=embed, files=[discord.File(logfname_detail)])
+                response=await message.reply(embed=embed, files=[discord.File(fp=logfname_detail, filename='detail.json')])
 
                 file='{0}/{1}'.format(
                     os.getcwd(),
